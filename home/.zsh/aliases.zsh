1e54a8ce824777a8268794f638f2b5853431a2bf
monzou <1984tkr@gmail.com>
2014-08-01 14:36:06 +0900

    Add git aliases

--- home/.zsh/aliases.zsh
+++ home/.zsh/aliases.zsh
@@ -1,7 +1,8 @@
 alias ll="ls -la"
 alias zz="source ~/.zshrc"
 
-alias gs="git status"
+alias gs="git status --short"
+alias gd="git diff"
 alias ga="git add -A"
 alias gc="git commit"
 alias gp="git push"
